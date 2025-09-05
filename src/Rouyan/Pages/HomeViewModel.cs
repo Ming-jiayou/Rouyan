6cc4c051221952233ec7b63530685448fd5ccb58
mingupup <2539823901@qq.com>
2025-09-05 18:53:36 +0800

    增加获取剪切板内容

--- src/Rouyan/Pages/HomeViewModel.cs
+++ src/Rouyan/Pages/HomeViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Windows;
 using Stylet;
 
 namespace Rouyan.Pages;
@@ -20,5 +21,35 @@ public class HomeViewModel : Screen
     public void NavigateToSettings()
     {
         this.navigationController.NavigateToSettings();
+    }
+    
+    public void GetClipboardContent()
+    {
+        try
+        {
+            if (Clipboard.ContainsText())
+            {
+                string clipboardText = Clipboard.GetText();
+                MessageBox.Show($"剪贴板内容：\n{clipboardText}", "剪贴板内容", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (Clipboard.ContainsImage())
+            {
+                MessageBox.Show("剪贴板中包含图片内容（非文本）", "剪贴板内容", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (Clipboard.ContainsFileDropList())
+            {
+                var files = Clipboard.GetFileDropList();
+                string fileList = string.Join("\n", files.Cast<string>());
+                MessageBox.Show($"剪贴板包含文件：\n{fileList}", "剪贴板内容", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("剪贴板为空或不包含可识别的内容类型", "剪贴板内容", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"获取剪贴板内容失败：{ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
