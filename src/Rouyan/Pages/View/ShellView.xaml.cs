446c087beeaf53e4691ce700200bd531f49a002d
mingupup <2539823901@qq.com>
2025-09-22 23:23:08 +0800

    实现最小化到托盘

--- src/Rouyan/Pages/View/ShellView.xaml.cs
+++ src/Rouyan/Pages/View/ShellView.xaml.cs
@@ -11,5 +11,44 @@ namespace Rouyan.Pages.View
         {
             InitializeComponent();
         }
+
+        private void Window_StateChanged(object sender, System.EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+                TrayIcon.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void TrayIcon_TrayLeftMouseDown(object sender, RoutedEventArgs e)
+        {
+            ShowMainWindow();
+        }
+
+        private void ShowWindow_Click(object sender, RoutedEventArgs e)
+        {
+            ShowMainWindow();
+        }
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            TrayIcon.Dispose();
+            Application.Current.Shutdown();
+        }
+
+        private void ShowMainWindow()
+        {
+            Show();
+            WindowState = WindowState.Normal;
+            Activate();
+            TrayIcon.Visibility = Visibility.Collapsed;
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            WindowState = WindowState.Minimized;
+        }
     }
 }
